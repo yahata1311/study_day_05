8eb5eefcdd1fc7bc468d3722e6df2aba0feac91c
yahata1311 <yahata1311@gmail.com>
2020-10-06 13:40:52 +0900

    いろいろﾌｧｲﾙ作った

---
+++ hello_from_CUI.py
@@ -0,0 +1,31 @@
+# Admin@VAIOpro11 MINGW64 ~/PycharmProjects/study_day_05 (main)
+# $ echo hello
+# hello と出力された
+#
+# わざとエラー
+# Admin@VAIOpro11 MINGW64 ~/PycharmProjects/study_day_05 (main)
+# $ echoBOb
+# bash: echoBOb: command not found　bachでは、コマンドが見つからないというエラー
+
+# touch hello.pyと入力すると、hello.pyのﾌｧｲﾙが新規作成される。touchはﾌｧｲﾙ更新/新規作成。
+# わざとエラー
+# $ touch　と入力
+# touch: missing file operand　（オペランド＝使い方）　touchの後にﾌｧｲﾙ名がないよ。
+# Try 'touch --help' for more information. ヘルプみてごらん。という意味。
+
+
+# catはﾌｧｲﾙの中身を出力する。
+# Admin@VAIOpro11 MINGW64 ~/PycharmProjects/study_day_05 (main)
+# $ cat hello.py
+#
+# Admin@VAIOpro11 MINGW64 ~/PycharmProjects/study_day_05 (main)
+# $ cat _from_CUI
+# cat: _from_CUI: No such file or directory # ﾌｧｲﾙもﾃﾞｨﾚｸﾄﾘｰもないです。
+#
+# Admin@VAIOpro11 MINGW64 ~/PycharmProjects/study_day_05 (main)
+# $
+
+# ls 現在のﾃﾞｨﾚｸﾄﾘｰ直下のﾌｧｲﾙとﾃﾞｨﾚｸﾄﾘｰを表示する。（ﾌｧｲﾙのﾌｫﾙﾀﾞｰが見れる　よく使う!！）
+# Admin@VAIOpro11 MINGW64 ~/PycharmProjects/study_day_05 (main)
+# $ ls
+# README.md  hello.py  hello_from_CUI.py  venv
